7bce03a6ca7474756c601d7de55647327163dc3d
BartoloCarnemolla <bartolo.carnemolla@gmail.com>
2022-01-20 12:30:41 +0100

    Prodotti Page grid system

--- pages/prodotti.js
+++ pages/prodotti.js
@@ -4,12 +4,12 @@ export default function ProdottiPage() {
   return (
     <Layout title='Prodotti'>
       <div>
-        <div className='row text-center g-2'>
-          <div className='col-md bg-secondary'>
-            <h3>Sidebar</h3>
+        <div className='row text-center g-0'>
+          <div className='col-xl-3 col-lg-4 col-md-3 col-sm-4 col-4 bg-white'>
+            <div className='h6 text-center text-dark'>CATEGORIE</div>
           </div>
-          <div className='col-md bg-warning'>
-            <h3>Content</h3>
+          <div className='col-xl-9 col-lg-8 col-md-9 col-sm-8 col-8 bg-light'>
+            <div className='h6 text-center text-success'>PRODOTTI</div>
           </div>
         </div>
       </div>
